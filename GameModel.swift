2ed8cc6ab3ad9864e63fb494fbc682da2b75e6c8
Guus <chunnanzi@me.com>
2016-12-26 14:07:12 +0800

    添加setPosition()方法
    存储方式

--- GameModel.swift
+++ GameModel.swift
@@ -48,4 +48,22 @@ class GameModel {
         }
         print("")
     }
+    
+    
+    //如果返回false，表示该位置已经有值
+    func setPosition(row:Int,col:Int,value:Int)->Bool{
+        assert(row >= 0 && row < dimension)
+        assert(col >= 0 && col < dimension)
+        //3行4列，即row=2,col=3 index=2*4+3 =11
+        //4行4列，即3*4+3=15
+        var index = self.dimension * row + col
+        var val = tiles[index]
+        if(val > 0){
+            print("该位置(\(row),\(col))已经有值了")
+            return false
+        }
+        
+        tiles[index] = value
+        return true
+    }
 }
